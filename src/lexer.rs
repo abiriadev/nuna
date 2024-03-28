c977fb6ed05c595544f1ecace649e958eef91ceb
Abiria <abiria.dev@gmail.com>
2024-03-28 14:48:03 +0900

    feat: implement `Lexer`

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,25 +1,53 @@
+use std::str::Chars;
+
 pub enum Token {
 	Push,
-	Mul,
-	Sub,
-	Add,
 	Pop,
-	Prev,
-	PopSub,
+	Add,
+	Sub,
+	Mul,
 	Pow,
-	Dots,
+	PopAdd,
+	PopSub,
+	Print,
+	Prev,
+	Dot,
 }
 
 pub struct Lexer<'s> {
-	source: &'s str,
-	position: usize,
+	source: Chars<'s>,
 }
 
 impl<'s> Lexer<'s> {
 	pub fn new(source: &'s str) -> Self {
 		Self {
-			source,
-			position: 0,
+			source: source.chars(),
 		}
+	}
+}
+
+impl<'s> Iterator for Lexer<'s> {
+	type Item = Result<Token, ()>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		let Some(c) = self.source.next() else {
+			return None;
+		};
+
+		Some(match c {
+			'눈' | '누' => Ok(Token::Push),
+			'난' | '나' => Ok(Token::Mul),
+			'주' => Ok(Token::Sub),
+			'거' => Ok(Token::Add),
+			'.' => Ok(Token::Dot),
+			'헤' => Ok(Token::Pop),
+			'으' => Ok(Token::Prev),
+			'응' => Ok(Token::PopSub),
+			'흐' => Ok(Token::Pow),
+			'읏' => unimplemented!(),
+			'💕' => Ok(Token::PopAdd),
+			'!' => Ok(Token::Print),
+			_ => Err(()),
+		})
 	}
 }
