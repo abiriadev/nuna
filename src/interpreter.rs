6fee2870c8d7996d16be6d850fa66668c7dca766
Abiria <abiria.dev@gmail.com>
2024-03-28 15:28:04 +0900

    feat: define output

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,23 +1,29 @@
+use std::io::Write;
+
 use crate::lexer::{Lexer, Token};
 
-pub struct Interpreter<Ts>
+pub struct Interpreter<Ts, O>
 where
 	Ts: Iterator<Item = Token>,
+	O: Write,
 {
 	token_stream: Ts,
 	stack: Vec<Option<i64>>,
 	pointer: Option<usize>,
+	output: O,
 }
 
-impl<Ts> Interpreter<Ts>
+impl<Ts, O> Interpreter<Ts, O>
 where
 	Ts: Iterator<Item = Token>,
+	O: Write,
 {
-	pub fn new(token_stream: Ts) -> Self {
+	pub fn new(token_stream: Ts, output: O) -> Self {
 		Self {
 			token_stream,
 			stack: Vec::new(),
 			pointer: None,
+			output,
 		}
 	}
 }
